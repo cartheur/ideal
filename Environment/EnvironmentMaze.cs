dc5d5d69b895cf87ceca89ef8a78b03184c7a24a
cartheur <cartheur@pm.me>
2024-12-02 21:28:23 +0100

    Start maze game

---
+++ Environment/EnvironmentMaze.cs
@@ -0,0 +1,14 @@
+﻿namespace Ideal.Environment
+{
+    /// <summary>
+    /// This class implements the Small Loop Environment
+    /// The Small Loop Problem: A challenge for artificial emergent cognition.
+    /// Olivier L.Georgeon, James B.Marshall.
+    /// BICA2012, Annual Conference on Biologically Inspired Cognitive Architectures.
+    /// Palermo, Italy. (October 31, 2012).
+    /// </summary>
+    public class EnvironmentMaze : Environment050
+    {
+
+    }
+}
