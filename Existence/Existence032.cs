b67476d796f910ebd75e0de3d1fc524a46d53a0a
cartheur <cartheur@pm.me>
2025-03-26 16:38:33 +0100

    Remove space

--- Existence/Existence032.cs
+++ Existence/Existence032.cs
@@ -8,7 +8,6 @@ namespace Ideal.Existence
     {
         public override string Step()
         {
-
             List<IAnticipation> anticipations = Anticipate();
             Interaction032 intendedInteraction = (Interaction032)SelectInteraction(anticipations);
             Experiment experience = intendedInteraction.GetExperience();
